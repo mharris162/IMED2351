e7eb74f3be17a19a4823b69b19ebdb7cf4e5e6a4
mharris162 <mharris162@student.alamo.edu>
2024-09-06 21:15:59 -0500

    assignment 2-html

--- assignmnet2/app.js
+++ assignmnet2/app.js
@@ -1,4 +1,4 @@
-var name = "mackenzie harris"; //website author
+var name = "mackenzie harris"; 
 console.log(name);
 
 var edit = 0; //global variable & starting value for edits
@@ -7,9 +7,17 @@ edit = edit +1; // edit total 1
 edit = edit +1; // edit total 2
 
 {
-    let x = 2: //block variable
-    let y = 7: //block variable
+    let x = 2; //block variable
+    let y = 7; //block variable
     let z = x + y; //block variable
+
+}
+
+//object variable using const
+const cat = {
+    name: "Louis", //string variable 
+    color: "tuxedo",
+    age: 9,
 
 }
 
