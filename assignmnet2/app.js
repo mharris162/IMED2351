3ad3687e4116b09e250d8c170f2084949bc9d5d0
mharris162 <mharris162@student.alamo.edu>
2024-09-06 20:07:45 -0500

    block variable

--- assignmnet2/app.js
+++ assignmnet2/app.js
@@ -1,3 +1,19 @@
-var name = "mackenzie harris" //website author
+var name = "mackenzie harris"; //website author
+console.log(name);
 
-console.log(name)
+var edit = 0; //global variable & starting value for edits
+
+edit = edit +1; // edit total 1
+edit = edit +1; // edit total 2
+
+{
+    let x = 2: //block variable
+    let y = 7: //block variable
+    let z = x + y; //block variable
+
+}
+
+
+
+
+
