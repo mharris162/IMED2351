f561fe0032f0ddd781c7c32529054ce6d0364550
mharris162 <mharris162@student.alamo.edu>
2024-09-12 20:19:50 -0500

    Function

--- assignment3/app.js
+++ assignment3/app.js
@@ -1,2 +1,38 @@
-const name = "MacKenzie Harris";
+const name = "MacKenzie Harris";// can be used later in code (global scope)
+
+function adder (num1, num2) { //() are the paramater 
+console.log (num1 + num2); //block scope; can only be used within function
+
+}
+
+
+adder (5,20); //function 1 end 
+
+
+function toUpper (text) {
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
+
+}
+
+toUpper (name); // function 2 end 
+
+
+const toUpper = function () {
+
+
+}
+
+toUpper (name); // function 3 end 
+
+
+const toUpper =  () => {
+
+
+}
+
+toUpper (name); // function 4 end 
+
+
+
 
